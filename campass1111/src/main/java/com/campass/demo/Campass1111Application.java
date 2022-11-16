bf756655035bf541e253e60a742b7cc76928ab7f
bona <leebona0301@gmail.com>
2022-11-16 12:38:46 +0900

    회원파트

--- campass1111/src/main/java/com/campass/demo/Campass1111Application.java
+++ campass1111/src/main/java/com/campass/demo/Campass1111Application.java
@@ -2,12 +2,19 @@ package com.campass.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Campass1111Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Campass1111Application.class, args);
 	}
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
