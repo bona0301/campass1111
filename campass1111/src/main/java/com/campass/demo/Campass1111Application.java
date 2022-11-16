14769b9e053dbde8c2dcaef1898c2f173928425a
bona <leebona0301@gmail.com>
2022-11-16 10:48:43 +0900

    초기설정 완료

---
+++ campass1111/src/main/java/com/campass/demo/Campass1111Application.java
@@ -0,0 +1,13 @@
+package com.campass.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Campass1111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Campass1111Application.class, args);
+	}
+
+}
---
+++ campass1111/src/test/java/com/campass/demo/Campass1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campass.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Campass1111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
